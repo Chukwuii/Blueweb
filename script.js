0e75ac7a26d58ba3aad8e06b7e2cdca04d1bb4aa
Chukwuii <109396213+Chukwuii@users.noreply.github.com>
2026-08-09 12:37:41 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
 function hello(){
 alert("Welcome!");
 }
+function subscribe(){
+
+    let email =
+    document.getElementById("email").value;
+
+
+    if(email===""){
+
+        alert("Please enter your email");
+
+    }
+
+    else{
+
+        alert(
+        "Thank you for subscribing: "
+        + email
+        );
+
+    }
+
+}
